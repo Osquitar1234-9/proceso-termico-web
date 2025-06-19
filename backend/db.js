7e71d07d317568ea13fdb55c7bafb8a5a4e0b238
Osquitar1234-9 <javier.decarpio.789@gmail.com>
2025-06-19 15:23:01 -0500

    cambios hoy

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-
-// Conexión a PostgreSQL desde Render
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-module.exports = pool;
